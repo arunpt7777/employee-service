d22e716b196e4316f4f06c6c7a7d1954a98286f3
Arun PT <arunpt7777@gmail.com>
2024-06-05 21:41:49 +0530

    Tested CRUD operations without throwing Exceptions for validations

--- employee-service/src/main/java/com/motta/employee_service/controller/EmployeeController.java
+++ employee-service/src/main/java/com/motta/employee_service/controller/EmployeeController.java
@@ -29,21 +29,21 @@ public class EmployeeController {
 		return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
 	}
 
-	// get Employee by id REST API
+	// Retrieve Employee by id REST API
 	@GetMapping("/employees/{id}")
 	public ResponseEntity<EmployeeDTO> retrieveEmployeeById(@PathVariable("id") Integer id) {
 		EmployeeDTO employee = employeeService.retrieveEmployeeById(id);
 		return new ResponseEntity<>(employee, HttpStatus.OK);
 	}
 
-	// Build Get All Employees REST API
+	// Retrieve All Employees REST API
 	@GetMapping("/employees")
 	public ResponseEntity<List<EmployeeDTO>> getAllEmployees() {
 		List<EmployeeDTO> employees = employeeService.getAllEmployees();
 		return new ResponseEntity<>(employees, HttpStatus.OK);
 	}
 
-	// Build Update Employee REST API
+	// Update Employee REST API
 	@PutMapping("/employees/{id}")
 	public ResponseEntity<EmployeeDTO> updateEmployee(@PathVariable("id") Integer id,
 			@RequestBody EmployeeDTO employee) {
@@ -52,44 +52,10 @@ public class EmployeeController {
 		return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
 	}
 
-	// Build Delete Employee REST API
-	@DeleteMapping("{id}")
+	// Delete Employee REST API
+	@DeleteMapping("/employees/{id}")
 	public ResponseEntity<String> deleteEmployee(@PathVariable("id") Integer id) {
 		employeeService.deleteEmployee(id);
 		return new ResponseEntity<>("Employee successfully deleted!", HttpStatus.OK);
 	}
-
-	/*
-	 * @GetMapping("/employees/{id}") public Employee
-	 * retrieveEmployeeById(@PathVariable Integer id) { Employee employee =
-	 * repository.findById(id).orElse(null); if (employee == null) throw new
-	 * UserNotFoundException("No such employee found with id = " + id); return
-	 * employee; }
-	 * 
-	 * @GetMapping("/employees") public List<Employee> retrieveAll() {
-	 * List<Employee> allEmployees = repository.findAll(); if
-	 * (allEmployees.isEmpty()) throw new
-	 * UserNotFoundException("No employee records found"); return allEmployees; }
-	 * 
-	 * @PostMapping("/employees") public ResponseEntity<Employee>
-	 * addEmployee(@RequestBody Employee emp) { emp.setId(++userCount); Employee
-	 * savedEmployee = repository.save(emp); URI location =
-	 * ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-	 * .buildAndExpand(savedEmployee.getId()).toUri(); return
-	 * ResponseEntity.created(location).build(); }
-	 * 
-	 * @DeleteMapping("/employees/{id}") public void
-	 * deleteEmployeeById(@PathVariable Integer id) { repository.deleteById(id); }
-	 * 
-	 * @PutMapping("/employees/{id}") public ResponseEntity<Employee>
-	 * updateEmployee(@PathVariable Integer id, @RequestBody Employee emp) {
-	 * Employee existingEmployee = repository.findById(id).get(); if
-	 * (existingEmployee == null) throw new
-	 * UserNotFoundException("No such employee found with id = " + id);
-	 * 
-	 * repository.deleteById(id); emp.setId(id); repository.save(emp); URI location
-	 * = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-	 * .buildAndExpand(existingEmployee.getId()).toUri(); return
-	 * ResponseEntity.created(location).build(); }
-	 */
 }
